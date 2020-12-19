51b6cc88baebdb6f14f1f83a89700d094fdb9d63
david060997 <david060997@gmail.com>
2020-12-18 22:47:45 -0600

    Estilos agregados, comienzo de pruebas para generar Factura.

--- src/main/java/com/timbrado/center_timbrado/services/Facturama.java
+++ src/main/java/com/timbrado/center_timbrado/services/Facturama.java
@@ -5,4 +5,5 @@ import com.Facturama.sdk_java.Container.FacturamaApi;
 public class Facturama {
 
 	public static FacturamaApi facturama = new FacturamaApi("ricardomangore", "1nt3rm3zz0", true);
+	
 }
